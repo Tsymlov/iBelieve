b9edfd7153fda442119bb9c37316e99978f76d10
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2014-12-16 13:31:45 +0300

    New project was created.

---
+++ iBelieve/iBelieve/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/16/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iBelieve/iBelieve/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/16/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
