687f3b1ffb0368e5813e0fd2b29d904c2c969776
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2014-12-18 13:43:44 +0300

    RestorePasswordView was added.

---
+++ iBelieve/iBelieve/RestorePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestorePasswordViewController.h
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/18/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestorePasswordViewController : UIViewController
+
+@end
---
+++ iBelieve/iBelieve/RestorePasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  RestorePasswordViewController.m
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/18/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+#import "RestorePasswordViewController.h"
+
+@interface RestorePasswordViewController ()
+
+@end
+
+@implementation RestorePasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
