d6837aebdd7f7c91ea0bbfc06ed93025a42c3d7c
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2014-12-18 14:02:13 +0300

    User class was created.

---
+++ iBelieve/iBelieve/User.h
@@ -0,0 +1,19 @@
+//
+//  User.h
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/18/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic, strong) NSString* firstName;
+@property (nonatomic, strong) NSString* lastName;
+@property (nonatomic, strong) NSString* password;
+@property (nonatomic, strong) NSString* email;
+@property (nonatomic, strong) NSString* userPicImagePath;
+
+@end
---
+++ iBelieve/iBelieve/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/18/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
