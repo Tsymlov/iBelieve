f71806a9d30ae1bc55a914355cc0bcc75e5c20e1
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2014-12-18 14:43:31 +0300

    Confessions enum was added. UI was polished.

---
+++ iBelieve/iBelieve/Confessions.h
@@ -0,0 +1,14 @@
+//
+//  Confessions.h
+//  iBelieve
+//
+//  Created by Alexey Tsymlov on 12/18/14.
+//  Copyright (c) 2014 iBelieve. All rights reserved.
+//
+
+enum confessions{
+    orthodoxy,
+    islam,
+    hebraism,
+    buddhism
+};
